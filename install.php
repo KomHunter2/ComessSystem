b057af6334d2bc3e9e2757130a7869671ea0bd2a
KomHunter2 <Dagobert1995@gmx.de>
2011-09-18 20:21:25 +0300

    Edited install.php

---
+++ install.php
@@ -0,0 +1,12 @@
+<?php
+   /**
+    * @package    ComessBook Installation
+    * @author     KomHunter <hunter@entenmix.de>
+    * @copyright  2011 KomHunter
+    * @license    http://creativecommons.org/licenses/by-sa/3.0/de/  CC BY-SA 3.0
+    */
+  
+	require_once 'config.inc.php';
+  require_once './templates/Smarty.class.php';
+	
+?>
